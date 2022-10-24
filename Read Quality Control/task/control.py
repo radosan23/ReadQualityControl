84840c6657481a83bf86bef0f3997a8c76f9917b
radosan <radoslaw.sanetra@gmail.com>
2022-10-24 18:53:01 +0200

    st4

--- Read Quality Control/task/control.py
+++ Read Quality Control/task/control.py
@@ -6,15 +6,17 @@ def get_data(path):
     return lines
 
 
-def print_results(r, ln, rep, gc):
+def print_results(r, ln, rep, gc, n, nps):
     print(f'Reads in the file = {len(r)}')
     print(f'Reads sequence average length = {ln}')
     print(f'\nRepeats = {rep}')
+    print(f'Reads with Ns = {n}')
     print(f'\nGC content average = {gc}%')
+    print(f'Ns per read sequence = {nps}%')
 
 
 def av_length(data):
-    lengths = dict()
+    lengths = {}
     for line in data:
         lengths[len(line)] = 1 if len(line) not in lengths else lengths[len(line)] + 1
     return round(sum(x * lengths[x] for x in lengths) / sum(lengths.values()))
@@ -27,8 +29,14 @@ def av_gc_cont(data):
     return round(sum(gc_cont) / len(gc_cont), 2)
 
 
+def ns_seq(data):
+    ns = [round(line.count('N') / len(line) * 100, 2) for line in data if 'N' in line]
+    return len(ns), round(sum(ns) / len(data), 2)
+
+
 reads = get_data(input())
 av_len = av_length(reads)
 av_GC = av_gc_cont(reads)
 repeats = len(reads) - len(set(reads))
-print_results(reads, av_len, repeats, av_GC)
+with_n, ns_per_seq = ns_seq(reads)
+print_results(reads, av_len, repeats, av_GC, with_n, ns_per_seq)
